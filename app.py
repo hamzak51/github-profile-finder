8ce1dcbc0a0c21f37f42acc79503fa262a158fbf
Hamza <hamza_k11@yahoo.com>
2025-08-10 20:56:38 +0500

    reversed change in app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 def github_profile():
     if request.method == "POST":
         username = request.form.get("username")
-        headers = {"Authorization": "token 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"}
-        userdetails=requests.get(f"https://api.github.com/users/{username}", headers=headers).json()
+        userdetails=requests.get(f"https://api.github.com/users/{username}").json()
         if "message" in userdetails and userdetails["message"] =="Not Found":
             return render_template('index.html', errorhtml="User not found!")
 
@@ -18,6 +17,4 @@ def github_profile():
     
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render/Heroku expects PORT env var
-    app.run(host="0.0.0.0", port=port, debug=True)
+app.run(debug=True)
