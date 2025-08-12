d762568324437dcfde99513ed7b78823bf02bb20
Hamza <hamza_k11@yahoo.com>
2025-08-12 23:45:47 +0500

    added gitignore

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request
 import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = Flask(__name__)
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
 @app.route("/", methods=['GET', 'POST'])
 def github_profile():
     if request.method == "POST":
         username = request.form.get("username")
-        userdetails=requests.get(f"https://api.github.com/users/{username}").json()
+
+        headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+        userdetails=requests.get(f"https://api.github.com/users/{username}", headers=headers).json()
+        
         if "message" in userdetails and userdetails["message"] =="Not Found":
             return render_template('index.html', errorhtml="User not found!")
 
@@ -17,4 +27,6 @@ def github_profile():
     
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
